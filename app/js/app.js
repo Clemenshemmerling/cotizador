c8514ab9ef797784891c903818a492a9fce2b5c8
Clemens Hemmerling <clemens@galileo.edu>
2017-12-07 17:52:06 -0600

    Cambios en estilo

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,15 @@
-$(document).ready(function () {
-  $('#inicio').pickadate()
-  $('#finaliza').pickadate()
+var app = new Vue({
+  el: ".app",
+  data: {
+    cantidad: 'Elije la cantidad de boletos',
+    counter: 0,
+    childConter: 0,
+  },
+  methods: {
+
+  },
+  mounted: function () {
+    $('#inicio').pickadate()
+    $('#finaliza').pickadate()
+  }
 })
