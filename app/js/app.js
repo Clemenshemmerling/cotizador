6d599753ed59be64a50c41f9a3f59727b5d5c04a
Clemens Hemmerling <clemens@galileo.edu>
2017-12-08 09:50:53 -0600

    Creacion de funciones para cotizador

--- app/js/app.js
+++ app/js/app.js
@@ -4,12 +4,32 @@ var app = new Vue({
     cantidad: 'Elije la cantidad de boletos',
     counter: 0,
     childConter: 0,
+    inicio: moment().format('DD'),
+    final: moment(Date.now(), "DD-MM-YYYY").add(5, 'days').format('DD')
   },
   methods: {
-
+    moreFirst: function () {
+      this.counter += 1;
+      this.cantidad = 'Adultos'+ ' '+ this.counter +' niños ' + this.childConter
+    },
+    minusFirst: function () {
+      this.counter -= 1;
+      this.cantidad = 'Adultos'+ ' '+ this.counter +' niños ' + this.childConter
+    },
+    cmoreFirst: function () {
+      this.childConter += 1;
+      this.cantidad = 'Adultos'+ ' '+ this.counter +' niños ' + this.childConter
+    },
+    cminusFirst: function () {
+      this.childConter -= 1;
+      this.cantidad = 'Adultos'+ ' '+ this.counter +' niños ' + this.childConter
+    },
   },
   mounted: function () {
-    $('#inicio').pickadate()
-    $('#finaliza').pickadate()
+    $('#inicio').pickadate();
+    $('#finaliza').pickadate();
+    $('.dropdown-content').click(function (event) {
+      event.stopPropagation();
+    });
   }
 })
